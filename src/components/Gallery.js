6980ae19e5ff9b058701dc6812dc29d88f18a8b2
Victor de Assis <victoralvespda@gmail.com>
2020-11-19 19:49:38 +0100

    Added the leaflet map.

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,13 +1,14 @@
-import React from "react";
-import NoImages from "./NoImages";
-import Image from "./Image";
-const Gallery = props => {
+import React from 'react';
+import NoImages from './NoImages';
+import Image from './Image';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+const Gallery = (props) => {
   const results = props.data;
   let images;
   let noImages;
   // map variables to each item in fetched image array and return image component
   if (results.length > 0) {
-    images = results.map(image => {
+    images = results.map((image) => {
       let farm = image.farm;
       let server = image.server;
       let id = image.id;
@@ -21,7 +22,28 @@ const Gallery = props => {
   }
   return (
     <div>
-      <ul>{images}</ul>
+      {images && (
+        <div>
+          <MapContainer
+            style={{ height: '500px' }}
+            center={[51.505, -0.09]}
+            zoom={13}
+            touchZoom={true}
+            scrollWheelZoom={true}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[51.505, -0.09]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+          <ul>{images}</ul>
+        </div>
+      )}
       {noImages}
     </div>
   );
