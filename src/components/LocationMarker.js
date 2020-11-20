758d5d560dff7dee8f0d5a5a4467ef291319b830
Victor de Assis <victoralvespda@gmail.com>
2020-11-20 20:22:19 +0100

    Refactored the code in the location marker component.

--- src/components/LocationMarker.js
+++ src/components/LocationMarker.js
@@ -1,15 +1,16 @@
+// eslint-disable-next-line
 import React, { useEffect } from 'react';
 import { useMapEvents } from 'react-leaflet';
 
 const LocationMarker = (props) => {
   const { mapCenterPosition } = props;
+  const map = useMapEvents({});
 
   useEffect(() => {
     const latLng = { lat: mapCenterPosition[0], lng: mapCenterPosition[1] };
     map.flyTo(latLng, map.getZoom());
-  }, [mapCenterPosition]);
+  }, [mapCenterPosition, map]);
 
-  const map = useMapEvents({});
   return null;
 };
 
